9d9ddd4b9f6185b0c24e2bbc5960cd536b0e4d56
2389451262@qq.com <2389451262@qq.com>
2022-04-28 00:52:40 +0800

    fix:格式化时间函数的bug修复

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Vue3-NetEaseCloudMusic
 Vue3+Electron+Ts
+
+# 当前实现接口
+> 接口地址（https://neteasecloudmusicapi.js.org/#/）
+1. 获取主页广告
+2. 获取推荐歌单（未登录） 
+3. 获取用户详情信息
--- src/utils/timestampToDate.ts
+++ src/utils/timestampToDate.ts
@@ -1,7 +1,10 @@
 export const timestampToDate = (timestamp: number) => {
     const date = new Date(timestamp)
-    const y = date.getFullYear()
-    const m = date.getMonth()
-    const d = date.getDay()
-    return y + "-" + m + "-" + d
+    const dateArr = date.toLocaleDateString().split("/")
+    if (parseInt(dateArr[1]) < 10) {
+        dateArr[1] = "0" + dateArr[1]
+    }
+    return dateArr.join("-")
+
+
 }
