bbfc290a5de1da8050f3e07115a3a5f9b586cc24
2389451262@qq.com <2389451262@qq.com>
2022-05-08 13:21:13 +0800

    修复冲突

--- src/utils/timestampToDate.ts
+++ src/utils/timestampToDate.ts
@@ -1,15 +1,8 @@
 export const timestampToDate = (timestamp: number) => {
     const date = new Date(timestamp)
-<<<<<<< HEAD
     const dateArr = date.toLocaleDateString().split("/")
     if (parseInt(dateArr[1]) < 10) {
         dateArr[1] = "0" + dateArr[1]
     }
     return dateArr.join("-")
-=======
-    const y = date.getFullYear()
-    const m = date.getMonth()
-    const d = date.getDay()
-    return y + "-" + m + "-" + d
->>>>>>> parent of adaecb3 (fix:a localeString time bug)
 }
